6162f9c59f84beca70a1162f8f39af283b65fdf8
DimMindfling <dimendroider@gmail.com>
2021-03-06 11:27:30 +0300

    Lesson15 task1 calculator works

--- Lesson17_task1/index.js
+++ Lesson17_task1/index.js
@@ -1,59 +1,56 @@
 'use strict';
 // Lesson17 task 1
+// https://codepen.io/dimfreelancerdn/pen/NWbBeoE
+// https://github.com/mindfling/GLO_JS17_Lesson17/tree/main/Lesson17_task1
 
 const inputA = document.querySelector('#a');
 const inputB = document.querySelector('#b');
-
 const buttonSum = document.querySelector('#sum');
 const buttonMult = document.querySelector('#mult');
 const inputRes = document.querySelector('#res');
 
-
-
 const calculator = {
-    a: 0,
-    b: 0,
-    res: 0,
+  a: 0,
+  b: 0,
+  res: 0,
+  sum: function () {
+    // this.res = this.a + this.b;
+    return this.a + this.b;
+  },
+  mult: function () {
+    return this.a * this.b;
+  },
+  show: function (event) {
+    if (inputA.value.trim() === '') {
+      inputA.value = 0;
+    }
+    if (inputB.value.trim() === '') {
+      inputB.value = 0;
+    }
+    this.a = +inputA.value;
+    this.b = +inputB.value;
 
-    sum: function () {
-        // this.res = this.a + this.b;
-        return this.a + this.b;
-    },
+    if (!isNaN(this.a) && !isNaN(this.b)) {
+      this.res = this.a + this.b;
 
-    mult: function () {
-        return this.a * this.b;
-    },
+      //в зависимости от id выбираем действие
+      if (event.target.id === 'sum') {
+        //суммирование
+        this.res = this.sum();
+      } else if (event.target.id === 'mult') {
+        //умножение
+        this.res = this.mult();
+      }
+    } else {
+      alert('Поля a и b должны быть числами');
+    }
 
-    show: function (event) {
-
-        if (inputA.value.trim() === '') {
-            inputA.value = 0;
-        }
-        if (inputB.value.trim() === '') {
-            inputB.value = 0;
-        }
-        this.a = +inputA.value;
-        this.b = +inputB.value;
-
-        if (!isNaN(this.a) && !isNaN(this.b)) {
-            this.res = this.a + this.b;
-            
-            //в зависимости от id выбираем действие
-            if (event.target.id === 'sum') {
-                //суммирование
-                this.res = this.sum();
-            } else if (event.target.id === 'mult') {
-                //умножение
-                this.res = this.mult();
-            }
-            
-        } else {
-            alert('Поля a и b должны быть числами');
-        }
-
-        inputRes.value = this.res;
-    },
+    inputA.value = this.a || 0;
+    inputB.value = this.b || 0;
+    inputRes.value = this.res;
+  },
 };
 
+// на обе кнопки навешиваем слушателя show()
 buttonSum.addEventListener('click', calculator.show.bind(calculator));
 buttonMult.addEventListener('click', calculator.show.bind(calculator));
